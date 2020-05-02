424fdf50b4869c8e4f0d31837703213949aed1a2
SagarBethi <saagarbethi@gmail.com>
2020-05-02 22:43:34 +0530

    Feedback Master Commit

--- src/main/java/com/feedback/app/controller/UserController.java
+++ src/main/java/com/feedback/app/controller/UserController.java
@@ -5,10 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +17,7 @@ import com.feedback.app.model.EmptyJson;
 import com.feedback.app.model.User;
 import com.feedback.app.service.UserService;
 
+@CrossOrigin(origins = "*",allowedHeaders = "*")
 @RestController
 public class UserController {
 	
